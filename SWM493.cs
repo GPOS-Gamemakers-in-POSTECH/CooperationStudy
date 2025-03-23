e4b14145c238174768be4ffd53038ebb398eeba9
3600DHMO <whjang3600@gmail.com>
2025-03-23 23:20:25 +0900

    Style SWM493(FLAVITY -> DoEuclieanAlgorithm, FACTORY -> GetLeastCommonMultiple)

--- SWM493.cs
+++ SWM493.cs
@@ -4,20 +4,20 @@ namespace WhatIsThisNamespace
 {
     public class SWM493
     {
-        public int FLAVITY(int num1, int num2)
+        public int DoEuclideanAlgorithm(int num1, int num2)
         {
             while (num2 != 0)
             {
                 int temp = num2;
-                num2 = num1 % num2;
+                num2 = num1 % num2; //if num1 < num2, replacing일 것이다.
                 num1 = temp;
             }
             return num1;
         }
 
-        public int FACTORY(int num1, int num2)
+        public int GetLeastCommonMultiple(int num1, int num2)
         {
-            return (num1 * num2) / FLAVITY(num1, num2);
+            return (num1 * num2) / DoEuclideanAlgorithm(num1, num2);
         }
     }
 }
