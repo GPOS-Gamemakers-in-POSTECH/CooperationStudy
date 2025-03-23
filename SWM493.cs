3668cd4fc334e84b715c884e2d512452108607c4
3600DHMO <whjang3600@gmail.com>
2025-03-23 23:03:01 +0900

    SWM493.FLAVITY(B->num2, C->temp), SWM493.FACTORY(Z->num1, Y->num2)

--- SWM493.cs
+++ SWM493.cs
@@ -4,20 +4,20 @@ namespace WhatIsThisNamespace
 {
     public class SWM493
     {
-        public int FLAVITY(int num1, int B)
+        public int FLAVITY(int num1, int num2)
         {
-            while (B != 0)
+            while (num2 != 0)
             {
-                int C = B;
-                B = num1 % B;
-                num1 = C;
+                int temp = num2;
+                num2 = num1 % num2;
+                num1 = temp;
             }
             return num1;
         }
 
-        public int FACTORY(int Z, int Y)
+        public int FACTORY(int num1, int num2)
         {
-            return (Z * Y) / FLAVITY(Z, Y);
+            return (num1 * num2) / FLAVITY(num1, num2);
         }
     }
 }
